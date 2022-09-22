60d3547cb4783333098e0d63372f8d62bf10dec3
Alisa <petrunina.alisa@gmail.com>
2022-09-22 21:10:12 +0300

    bot

--- task2_sem5.py
+++ task2_sem5.py
@@ -11,45 +11,76 @@
 
 
 # вариант человек против человека:
+# from random import randint
+
+# def input_dat(name):
+#     x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+#     while x < 1 or x > 28:
+#         x = int(input(f"{name}, введите корректное количество конфет: "))
+#     return x
+
+
+# def p_print(name, k, counter, value):
+#     print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
+
+# player1 = input("Введите имя первого игрока: ")
+# player2 = input("Введите имя второго игрока: ")
+# value = int(input("Введите количество конфет на столе: "))
+# flag = randint(0,2) # флаг очередности
+# if flag:
+#     print(f"Первый ходит {player1}")
+# else:
+#     print(f"Первый ходит {player2}")
+
+# counter1 = 0 
+# counter2 = 0
+
+# while value > 28:
+#     if flag:
+#         k = input_dat(player1)
+#         counter1 += k
+#         value -= k
+#         flag = False
+#         p_print(player1, k, counter1, value)
+#     else:
+#         k = input_dat(player2)
+#         counter2 += k
+#         value -= k
+#         flag = True
+#         p_print(player2, k, counter2, value)
+
+# if flag:
+#     print(f"Выиграл {player1}")
+# else:
+#     print(f"Выиграл {player2}")
+
+# Против бота
 from random import randint
-
-def input_dat(name):
-    x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
-    while x < 1 or x > 28:
-        x = int(input(f"{name}, введите корректное количество конфет: "))
-    return x
-
-
-def p_print(name, k, counter, value):
-    print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
-
-player1 = input("Введите имя первого игрока: ")
-player2 = input("Введите имя второго игрока: ")
-value = int(input("Введите количество конфет на столе: "))
-flag = randint(0,2) # флаг очередности
-if flag:
-    print(f"Первый ходит {player1}")
-else:
-    print(f"Первый ходит {player2}")
-
-counter1 = 0 
-counter2 = 0
-
-while value > 28:
-    if flag:
-        k = input_dat(player1)
-        counter1 += k
-        value -= k
-        flag = False
-        p_print(player1, k, counter1, value)
+candies = 200
+print(f'{candies} всего конфет')
+count = randint(1, 2)
+while candies > 0:
+    count += 1
+    if count % 2 == 0:
+        if count == 2:
+            print('ходит бот')
+            quantity = 20
+            print(f'конфет изымаете бот {quantity}')
+        else:
+            print('ходит бот')
+            quantity = 29 - quantity
+            print(f'конфет изымаете бот {quantity}')
     else:
-        k = input_dat(player2)
-        counter2 += k
-        value -= k
-        flag = True
-        p_print(player2, k, counter2, value)
+        print('ходит игрок')
+        quantity = int(input('Введите число конфет которое изымаете - '))
+    if 0 < quantity < 29:
+        candies -= quantity
+        print(f'конфет осталось - {candies}')
+    else:
+        print('Вы ввели некоректное кол-во конфет, введите число от 1 до 28')
+        count -= 1
 
-if flag:
-    print(f"Выиграл {player1}")
+if count % 2 == 0:
+    print('победил бот')
 else:
-    print(f"Выиграл {player2}")
+    print('победил игрок')
